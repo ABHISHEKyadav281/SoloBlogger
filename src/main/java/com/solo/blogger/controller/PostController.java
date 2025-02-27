5bbcef1a56ece3baaf34503d371f1b6745a167af
ABHISHEKyadav281 <yyadavabhishek9@gmail.com>
2025-02-27 23:08:11 +0530

    achieved multithreading for addPost.

--- src/main/java/com/solo/blogger/controller/PostController.java
+++ src/main/java/com/solo/blogger/controller/PostController.java
@@ -9,40 +9,48 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
-@RequestMapping(value = "/post/v1")
+@RequestMapping(value = "/post")
 public class PostController {
 
     @Autowired
     private PostService postService;
 
-    @PostMapping(value = "/addPost")
+    @PostMapping(value = "/v1/addPost")
     public ResponseEntity<?>AddPost(@RequestBody PostDto postDto){
         System.out.println("add post controller called");
         Post savedPost=postService.addPost(postDto);
         return ResponseEntity.ok("Post created successfully!");
     }
 
-    @GetMapping(value = "/getAllPosts")
+    @PostMapping(value = "/v2/addPost")
+    public CompletableFuture<ResponseEntity<Post>>AddPostV2(@RequestBody PostDto postDto){
+        System.out.println("add post controller called");
+        return postService.addPostV2(postDto)
+                .thenApply(ResponseEntity::ok);
+    }
+
+    @GetMapping(value = "/v1/getAllPosts")
     public ResponseEntity<?>AllPosts(){
         List<PostDto> allPosts=postService.getAllPost();
         return ResponseEntity.ok(allPosts);
     }
 
-    @PostMapping(value = "/getPostById")
+    @PostMapping(value = "/v1/getPostById")
     public ResponseEntity<?>PostById(@RequestBody GetPostDto getPostDto){
         PostDto userPost=postService.PostById(getPostDto);
         return ResponseEntity.ok(userPost);
     }
 
-    @PostMapping(value = "/getPostByUserId")
+    @PostMapping(value = "/v1/getPostByUserId")
     public ResponseEntity<?>PostByUserId(@RequestBody GetPostDto getPostDto){
         List<PostDto> userPost=postService.PostByUserId(getPostDto);
         return ResponseEntity.ok(userPost);
     }
 
-    @DeleteMapping(value= "/deletePostById")
+    @DeleteMapping(value= "/v1/deletePostById")
     public ResponseEntity<?> deletePost(@RequestBody GetPostDto getPostDto){
         postService.deletePost(getPostDto);
         return ResponseEntity.ok("post deleted Successfully");
