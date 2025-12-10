18645427691f9f628f4c1d6afc9b88354abf3efd
ABHISHEKyadav281 <yyadavabhishek9@gmail.com>
2025-12-11 02:24:47 +0530

    user actions(subscribe)

--- src/main/java/com/solo/blogger/controller/UserActionController.java
+++ src/main/java/com/solo/blogger/controller/UserActionController.java
@@ -2,6 +2,7 @@ package com.solo.blogger.controller;
 
 import com.solo.blogger.dto.ApiResponseDto;
 import com.solo.blogger.dto.apiResponse.PostResponseDto;
+import com.solo.blogger.entity.User;
 import com.solo.blogger.service.UserActionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,18 +17,6 @@ public class UserActionController {
     @Autowired
     private UserActionService userActionService;
 
-//    @PostMapping("/subscribe/{subscriberId}/{bloggerId}")
-//    public ResponseEntity<?> subscribe(@PathVariable Long subscriberId, @PathVariable Long bloggerId){
-//        System.out.println("hii");
-//        subscriptionService.subscribe(subscriberId,bloggerId);
-//        return ResponseEntity.ok(ApiResponseDto.success("Subscribed succesfully"));
-//    }
-
-//    @DeleteMapping("unSubscribe/{subscriberId}/{bloggerId}")
-//    public ResponseEntity<?> unSubscribe(@PathVariable Long subscriberId, @PathVariable Long bloggerId){
-//        subscriptionService.unSubscribe(subscriberId,bloggerId);
-//        return ResponseEntity.ok(ApiResponseDto.success("unSubscribed successfully"));
-//    }
 
     @PostMapping("/bookmark")
     public ResponseEntity<?> bookmarkPost(@RequestParam Long postId,
@@ -40,6 +29,33 @@ public class UserActionController {
     public ResponseEntity<?> getBookmarkedPosts(@RequestAttribute("userId") Long userId) {
         List<PostResponseDto> response=userActionService.getBookmarkedPosts(userId);
         return ResponseEntity.ok(ApiResponseDto.success(response));
+    }
+
+    @PostMapping("/unbookmark")
+    public ResponseEntity<?> unBookmarkPost(@RequestParam Long postId,
+                                          @RequestAttribute("userId") Long userId) {
+        userActionService.unBookmarkPost(postId,userId);
+        return ResponseEntity.ok(ApiResponseDto.success("Post unBookmarked successfully"));
+    }
+
+    @PostMapping("/subscribe")
+    public ResponseEntity<?> subscribe(@RequestParam Long bloggerId,
+                                            @RequestAttribute("userId") Long userId) {
+        userActionService.subscribeBlogger(bloggerId,userId);
+        return ResponseEntity.ok(ApiResponseDto.success("Subscribed user successfully"));
+    }
+
+    @PostMapping("/unsubscribe")
+    public ResponseEntity<?> unSubscribe(@RequestParam Long bloggerId,
+                                       @RequestAttribute("userId") Long userId) {
+        userActionService.unSubscribeBlogger(bloggerId,userId);
+        return ResponseEntity.ok(ApiResponseDto.success("unSubscribed user successfully"));
+    }
+
+    @GetMapping("/my/subscribers")
+    public ResponseEntity<?> getSubscribers(@RequestAttribute("userId") Long userId) {
+       List<String> mySubscribersList= userActionService.subscribersList(userId);
+        return ResponseEntity.ok(ApiResponseDto.success(mySubscribersList));
     }
 
 }
