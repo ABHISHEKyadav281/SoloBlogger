49e398e334758de695f49b61fcad94ac5c36a9db
ABHISHEKyadav281 <yyadavabhishek9@gmail.com>
2026-03-07 04:52:05 +0530

    post images uploaded to s3

--- src/main/java/com/solo/blogger/service/S3FileStorageService.java
+++ src/main/java/com/solo/blogger/service/S3FileStorageService.java
@@ -9,7 +9,6 @@ import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import jakarta.annotation.PostConstruct;
-import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
